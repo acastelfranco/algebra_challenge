c344c93b4c12bac873453464e3b7ca3e3575bd88
Antonino Castelfranco <acastelfranco@gmail.com>
2021-12-08 21:32:00 +0100

    Leave just simple mathematical functions into Math.h

--- include/Math.h
+++ include/Math.h
@@ -3,27 +3,6 @@
 namespace math {
 
     /**
-     * @brief Implement a generic side of a polygon.
-     * We have just the lenght of the side for now.
-     * 
-     * Note: it could be used a Segment as a Side,
-     * but for now I would not like to add this dependency.
-     * These three structs are just to implement the right
-     * overloading for the hypotenuse functions .
-     */
-    struct Side { double lenght; };
-
-    /**
-     * @brief Implement the opposite side of a right triangle
-     */
-    struct Opposite : public Side { };
-
-    /**
-     * @brief Implement the adjacent side of a right triangle
-     */
-    struct Adjacent : public Side { }; 
-
-    /**
      * @brief Returns whether two numbers are close within an epsilon.
      * 
      * @param a 
@@ -35,24 +14,6 @@ namespace math {
 
     bool
     close(double a, double b, double epsilon = 1e-4);
-
-    /**
-     * @brief A right triangle has one angle with a value of 90 degrees
-     * (M_PI / 2 radians).
-     * 
-     * The three trigonometric functions most often used to solve for
-     * a missing side of a right triangle are:
-     * 
-     * sin(t) = opposite / hypotenuse
-     * cos(t) = adjacent / hypotenuse
-     * tan(t) = opposite / adjacent
-     * 
-     * Plus the tangent can be expressed using sine and cosine functions:
-     * 
-     * tan(t) = sin(t)   / cos(t)
-     * 
-     * t is the "tilt" angle in radians.
-     */
 
     /**
      * @brief Converts angle in degrees into angle in radians
@@ -75,64 +36,4 @@ namespace math {
     radToDeg(double angleRad) {
         return angleRad * 180.0 / M_PI;
     }
-
-    /**
-     * @brief This function calculates the adjacent side(base) of a right triangle
-     * 
-     * @param tiltAngleDeg 
-     * @param hypotenuse 
-     * @return double 
-     */
-    double
-    adjacent(double tiltAngleDeg, double hypotenuse);
-
-    /**
-     * @brief This function calculates the opposite side(height) of a right triangle
-     * 
-     * @param tiltAngleDeg 
-     * @param hypotenuse 
-     * @return double 
-     */
-    double
-    opposite(double tiltAngleDeg, double hypotenuse);
-
-    /**
-     * @brief Calculates the triangle hypotenuse tilt angle
-     * 
-     * @param opposite 
-     * @param adjacent 
-     * @return double 
-     */
-    double
-    tiltAngleDeg(double opposite, double adjacent);
-
-    /**
-     * @brief Calculates the hypotenuse
-     * 
-     * @param opposite 
-     * @param adjacent 
-     * @return double 
-     */
-    double
-    hypotenuse(double opposite, double adjacent);
-
-    /**
-     * @brief 
-     * 
-     * @param tiltAngleDeg 
-     * @param opposite 
-     * @return double 
-     */
-    double
-    hypotenuse(double tiltAngleDeg, Opposite &opposite);
-
-    /**
-     * @brief 
-     * 
-     * @param tiltAngleDeg 
-     * @param adjacent 
-     * @return double 
-     */
-    double
-    hypotenuse(double tiltAngleDeg, Adjacent &adjacent);
 }
--- src/Math.cpp
+++ src/Math.cpp
@@ -6,39 +6,3 @@ math::close(double a, double b, double epsilon)
 {
     return std::fabs(a - b) < epsilon;
 }
-
-double
-math::adjacent(double tiltAngleDeg, double hypotenuse)
-{
-    return std::cos(degToRad(tiltAngleDeg)) * hypotenuse;
-}
-
-double
-math::opposite(double tiltAngleDeg, double hypotenuse)
-{
-    return std::sin(degToRad(tiltAngleDeg)) * hypotenuse;
-}
-
-double
-math::tiltAngleDeg(double opposite, double adjacent)
-{
-    return radToDeg(std::atan2(opposite, adjacent));
-}
-
-double
-math::hypotenuse(double opposite, double adjacent)
-{
-    return std::sqrt((opposite * opposite) + (adjacent * adjacent));
-}
-
-double
-math::hypotenuse(double tiltAngleDeg, Opposite &opposite)
-{
-    return opposite.lenght / std::sin(degToRad(tiltAngleDeg));
-}
-
-double
-math::hypotenuse(double tiltAngleDeg, Adjacent &adjacent)
-{
-    return adjacent.lenght / std::cos(degToRad(tiltAngleDeg));
-}
